e0dfb6232e3d0a810065a6148fa7856db2eda9cf
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-08-25 09:53:18 -0500

    added shuffled/unshuffled routes using python random module

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import random
 
 app = Flask(__name__)
+
+cards = [
+    "1H",
+    "2H",
+    "3H",
+    "4H",
+    "5H",
+    "6H",
+    "7H",
+    "8H",
+    "9H",
+    "10H",
+    "JH",
+    "QH",
+    "KH"
+]
 
 @app.route("/")
 def test ():
     return "Basic Functionality Running"
+
+@app.route('/deck')
+def deck():
+    newDeck = random.sample(cards, len(cards))
+    return jsonify(newDeck)
+
+@app.route('/unshuffled')
+def unshuffled():
+    return jsonify(cards)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
